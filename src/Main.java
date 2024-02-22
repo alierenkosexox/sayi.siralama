bcca3c5b943d1ea9ec6e1b2bc018e0867064cebd
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-22 23:24:19 +0300

    comments writing and bug fixed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a, b, c;                        //a,b,c değerlerini oluştur
+
+        Scanner input = new Scanner(System.in);         //scanner oluşturma
+
+        System.out.println("1.= a sayısını giriniz : ");       // 1. sayıyı iste ve a değerine kaydet
+        a = input.nextInt();
+
+        System.out.println("2.= b sayısını giriniz : ");       //2. sayıyı iste ve b değerine kaydet
+        b = input.nextInt();
+
+        System.out.println("3.= c sayısını giriniz ");          // 3. sayıyı iste ve c değerine kaydet
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {               // a nın en büyük olduğu senaryoda if ile b ve c nin tüm durumlarını
+            if (b > c) {                         // belirle ve durumuna göre sıralamyı yazdır
+                System.out.println("a > b > c");
+            } else if (c > b) {
+                System.out.println("a > c > b");
+            }
         }
-    }
-}
+        if ((b > a) && (b > c)) {                  // b nin en büyük olduğu senaryoda diğer olasılıkları belirle ve yazdır
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else if (c > a) {
+                System.out.println("b > c > a");
+            }
+        }
+            if ((c > b) && (c > a)) {               // c nin en büyük olduğu senaryoyu belirle ve yazdır
+                if (a > b) {
+                    System.out.println("c > a > b");
+                } else if( b > a ) {
+                    System.out.println("c > b > a");
+                }
+            }
+        }
+    } // Ali Eren KÖSE  22/02/2024  23:18
